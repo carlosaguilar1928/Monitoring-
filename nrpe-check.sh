662d8e8c4ef8a9a68a23ad95474c934953b2e837
carlosaguilar1928 <carlosaguilar1928@gmail.com>
2018-05-24 19:07:59 -0700

    Update nrpe-check.sh

--- nrpe-check.sh
+++ nrpe-check.sh
@@ -2,6 +2,8 @@
 # This is a script meant to let us test NRPE
 # Plugins that we write
 
+cycle=$( uptime  | awk '{print $10}' | awk -F "," '{print $1}'
+
 status="0"                                  # Change the status to test different alert states
 
 if [ $status == "0" ]; then
